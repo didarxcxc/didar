2a5694e38d5ef7679a3d4849b7945514f9e773b2
didarxcxc <146726965+didarxcxc@users.noreply.github.com>
2023-11-07 17:25:20 +0600

    Create hw0611.js

---
+++ hw0611.js
@@ -0,0 +1,174 @@
+// 1
+// let challenge = "30 days of Javascript"
+// console.log(challenge);
+// 2
+// let a = "30 days of Javascript"
+// console.log(a);
+// 3
+// let a = "30 days of Javascript"
+// console.log(a.length);
+// 4
+// let a = "30 days of Javascript"
+// console.log(a.toUpperCase())
+// 5
+// let a = "30 days of Javascript"
+// console.log(a.toLowerCase())
+// 6
+// let b = "30 days of Javascript";
+// let firstWord = b.substring(0, b.indexOf(' '));
+// console.log(firstWord);
+// 7
+// let d = "30 days of Javascript";
+// let result = d.replace("days of Javascript", "");
+// console.log(result);
+// 8
+// const str = "OMG poco.";
+// const soz = "Script";
+// if (str.includes(soz)) {
+//   console.log("Стрингде '" + soz + "' найдено.");
+// } else {
+//   console.log("Стрингде '" + soz + "' не найлено.");
+// }
+// 9
+// const a = "This is a sample string that we want to split";
+// const b = a.split(' '); 
+// console.log(b);
+// 10
+// const a = "30 Days of Javascript";
+// const b = a.split(' ');
+// console.log(b)
+// 11
+// const a = "Facebok, Google, Microsoft, Apple, IBM, Amazonacle, Amazon"
+// const b = a.split(', ')
+// console.log(b)
+// 12
+// let a = "30 Days Of Javascript";
+// a = a.replace("Javascript", "Python");
+// console.log(a)
+// 13
+// let a = "30 Days of Javascript";
+// let b = a.charAt(18);
+// console.log(b)
+// 14
+// let a = "30 Days Of JavaScript";
+// let b = a.charCodeAt(15);
+// console.log(b)
+// 15
+// let a = "30 Days Of JavaScript"
+// let b = a.indexOf("J");
+// console.log(b)
+// 16
+// let a = "30 Days Of JavaScript"
+// let b = a.lastIndexOf("S")
+// console.log(b)
+// 17
+// let a = "30 Days of javascript";
+// let b = a.indexOf("javascript");
+// console.log(b);
+// 18
+// let a = "30 Days of javascript";
+// let b = a.lastIndexOf("Days");
+// console.log(b);
+// 19
+// let a = "30 Days of javascript";
+// let b = a.search("javascript");
+// console.log(b);
+// 20
+// let a = '30 Days Of javascript';
+// let b = a.trim();  
+// console.log(b);
+// 21
+// let a = "30 Days of Javascript";
+// let b = a.startsWith("30 Days");
+// console.log(b)
+// 22
+// let a = "30 Days of Javascript";
+// let b = a.endsWith("30 Days");
+// console.log(b);
+// 23
+// let a = "30 Days of Javascript";
+// let b = a.match();
+// console.log(b);
+// 24
+// let a = "30 Days";
+// let b = "Javascript";
+// let v = a+b;
+// console.log(v);
+// 25
+// let a = "30 Days Javascript";
+// let b = a.repeat(2);
+// console.log(b)
+// const agree = confirm('18 barma?')
+// agree
+//     ?console.log("kir")
+//     :console.log("kury")
+// const now = new Date()
+// console.log(now.getFullYear())
+// 1
+// let firstName = "Didar";
+// let lastName = "Ayupov";
+// let country = "ALmaty";
+// let city = "Almaty";
+// let age = 13;
+// let isMarried = false;
+// let year = 2010;
+// console.log(typeof firstName); 
+// console.log(typeof lastName);  
+// console.log(typeof country);   
+// console.log(typeof city);      
+// console.log(typeof age);       
+// console.log(typeof isMarried); 
+// console.log(typeof year);   
+// console.log(typeof '10' === typeof 10); 
+// 2
+// console.log(typeof '10' === typeof 10);
+// 3
+// console.log(parseInt('9.8') === 10);
+// 4
+// let truthyStatement1 = 5 > 3;
+// let truthyStatement2 = "rok" !== "kk";
+// let truthyStatement3 = !!'koeeee';
+// 5
+// console.log(4 > 3);   
+// console.log(4 >= 3);  
+// console.log(4 < 3);    
+// console.log(4 <= 3);   
+// console.log(4 == 4);  
+// console.log(4 === 4); 
+// console.log(4 != 4);   
+// console.log(4 !== 4);  
+// console.log(4 != '4'); 
+// console.log(4 == '4');
+// console.log(4 === '4');
+// let python = 'python';
+// let jargon = 'jargon';
+// 6
+// console.log(python.length == jargon.length);
+// console.log(4 > 3 && 10 < 12);   
+// console.log(4 > 3 && 10 > 12);    
+// console.log(4 > 3 || 10 < 12);   
+// console.log(4 > 3 || 10 > 12);   
+// console.log(!(4 > 3));            
+// console.log(!(4 < 3));           
+// console.log(!(false));           
+// console.log(!(4 > 3 && 10 < 12)); 
+// console.log(!(4 > 3 && 10 > 12));
+// console.log(!(4 === '4'));
+// let dragon = 'dragon';
+// let python = 'python';
+// 7
+// let today = new Date();
+// let year = today.getFullYear();
+// let month = today.getMonth() + 1; 
+// let date = today.getDate();
+// let day = today.getDay();
+// let hours = today.getHours();
+// let minutes = today.getMinutes();
+// let secondsSince1970 = today.getTime() / 1000;
+// console.log('Year:', year);
+// console.log('Month:', month);
+// console.log('Date:', date);
+// console.log('Day:', day);
+// console.log('Hours:', hours);
+// console.log('Minutes:', minutes);
+// console.log('Seconds since January 1, 1970:', secondsSince1970);
